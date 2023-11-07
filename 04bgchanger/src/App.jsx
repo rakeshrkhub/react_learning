8a2017a05278c962c74aaafd67c92d28e3878336
rakeshrkhub <rakeshkumarrambad@gmail.com>
2023-11-08 02:50:09 +0530

    background-chnager Added

---
+++ 04bgchanger/src/App.jsx
@@ -0,0 +1,91 @@
+import {useState}  from 'react'
+
+function App() {
+  const [color,setColor] = useState("olive")
+
+  return (
+    <div className="w-full h-screen duration-200" 
+    style={{backgroundColor:color}}
+    >
+      <div className="fixed flex flex-wrap justify-center 
+      bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-centre 
+        gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+        <button 
+        onClick={()=>setColor("red")}
+        className="outline-none px-10 py-1 
+        rounded-full text-white shadow-lg"
+        style={{backgroundColor:"red"}}
+        >Red</button>
+
+        <button 
+        onClick={()=>setColor("green")}
+        className="outline-none px-10 py-1 
+        rounded-full text-white shadow-lg"
+        style={{backgroundColor:"green"}}
+        >Green</button>
+
+        <button 
+        onClick={()=>setColor("blue")}
+        className="outline-none px-10 py-1 
+        rounded-full text-white shadow-lg"
+        style={{backgroundColor:"blue"}}
+        >Blue</button>
+
+         <button 
+        onClick={()=>setColor("violet")}
+        className="outline-none px-10 py-1 
+        rounded-full text-white shadow-lg"
+        style={{backgroundColor:"violet"}}
+        >Red</button>
+
+         <button 
+        onClick={()=>setColor("yellow")}
+        className="outline-none px-10 py-1 
+        rounded-full text-white shadow-lg"
+        style={{backgroundColor:"yellow"}}
+        >Red</button>
+
+         <button 
+        onClick={()=>setColor("orange")}
+        className="outline-none px-10 py-1 
+        rounded-full text-white shadow-lg"
+        style={{backgroundColor:"orange"}}
+        >Red</button>
+
+         <button 
+        onClick={()=>setColor("black")}
+        className="outline-none px-10 py-1 
+        rounded-full text-white shadow-lg"
+        style={{backgroundColor:"black"}}
+        >Red</button>
+
+         <button 
+        onClick={()=>setColor("purple")}
+        className="outline-none px-10 py-1 
+        rounded-full text-white shadow-lg"
+        style={{backgroundColor:"purple"}}
+        >Red</button>
+
+         <button 
+        onClick={()=>setColor("brown")}
+        className="outline-none px-10 py-1 
+        rounded-full text-white shadow-lg"
+        style={{backgroundColor:"brown"}}
+        >Red</button>
+
+         <button 
+        onClick={()=>setColor("grey")}
+        className="outline-none px-10 py-1 
+        rounded-full text-white shadow-lg"
+        style={{backgroundColor:"grey"}}
+        >Red</button>
+        </div>
+      </div>
+    </div>
+    
+  )
+}
+
+export default App
+
